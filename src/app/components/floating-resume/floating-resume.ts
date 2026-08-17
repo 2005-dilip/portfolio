420bf0c3e59157d3fac299861cb208a5d9e9d031
2005-dilip <dilipkumar.26291111@gmail.com>
2026-08-17 15:11:27 +0530

    Add local resume PDF to public folder for instant download

--- src/app/components/floating-resume/floating-resume.ts
+++ src/app/components/floating-resume/floating-resume.ts
@@ -14,12 +14,5 @@ export class FloatingResumeComponent {
     return this.ps.profile().resumeUrl || 'https://drive.google.com/file/d/1SgCFXdRm7x_omH08BvqMU_s_luNVBTM5/view?usp=sharing';
   });
 
-  readonly downloadUrl = computed(() => {
-    const url = this.resumeUrl();
-    const match = url.match(/\/d\/([a-zA-Z0-9_-]+)/);
-    if (match && match[1]) {
-      return `https://drive.google.com/uc?export=download&id=${match[1]}`;
-    }
-    return url;
-  });
+  readonly localDownloadUrl = 'Dilip_Kumar_S_Resume.pdf';
 }
